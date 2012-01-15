0fff8bcd31dfb0cdbbd207e02cd97b3613abd27c
Jeff Rock <jeff@mobelux.com>
2012-01-15 16:49:14 -0500

    Linked up the user.

--- end.js
+++ end.js
@@ -3,14 +3,27 @@ $.fn.outerHTML = function() {
 };
 
 function getMediaID(theMessageEvent) {
-	if (theMessageEvent.name === "theMediaID") {
-		var mediaID = theMessageEvent.message;
+	if (theMessageEvent.name === "theInstagramData") {
+		var mediaID = theMessageEvent.message.media_id;
+		var authorID = theMessageEvent.message.author_id
 		
 		// Link the photo with the URL scheme
 		var currentPhotoHTML = $('img.photo:first').outerHTML();
 		var newPhotoHTML = '<a href=\"x-mobelux-carousel://openmedia?mediaID=' + mediaID + '\" title=\"View photo in Carousel\">' + currentPhotoHTML + '</a>';
 		$('img.photo:first').replaceWith(newPhotoHTML);
 		
+		// Get username and link with the URL scheme
+		var usernameElement = $('.username:first');
+		var usernameText = usernameElement.text();
+		var currentAuthorNameHTML = usernameElement.outerHTML();
+		var newAuthorNameHTML = '<a href=\"x-mobelux-carousel://openuser?userID=' + authorID + '\" title=\"View ' + usernameText + ' in Carousel\">' + currentAuthorNameHTML + '</a>';
+		usernameElement.replaceWith(newAuthorNameHTML);
+		//alert(currentAuthorNameHTML);
+		
+		// Link the author avatar with the URL scheme
+		var currentAuthorHTML = $('img.profile-picture:first').outerHTML();
+		var newAuthorHTML = '<a href=\"x-mobelux-carousel://openuser?userID=' + authorID + '\" title=\"View ' + usernameText + ' in Carousel\">' + currentAuthorHTML + '</a>';
+		$('img.profile-picture:first').replaceWith(newAuthorHTML);
    }
 }
 
