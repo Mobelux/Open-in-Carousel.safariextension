ae522179529f1fdaf3d332b75afca23e281062af
Jeff Rock <jeff@mobelux.com>
2012-06-29 11:26:55 -0400

    Updated safari extension.

--- View in Carousel.safariextension/end.js
+++ View in Carousel.safariextension/end.js
@@ -12,18 +12,10 @@ function getMediaID(theMessageEvent) {
 		var newPhotoHTML = '<a href=\"x-mobelux-carousel://openmedia?mediaID=' + mediaID + '\" title=\"View photo in Carousel\">' + currentPhotoHTML + '</a>';
 		$('img.photo:first').replaceWith(newPhotoHTML);
 		
-		// Get username and link with the URL scheme
-		var usernameElement = $('.username:first');
-		var usernameText = usernameElement.text();
-		var currentAuthorNameHTML = usernameElement.outerHTML();
-		var newAuthorNameHTML = '<a href=\"x-mobelux-carousel://openuser?userID=' + authorID + '\" title=\"View ' + usernameText + ' in Carousel\">' + currentAuthorNameHTML + '</a>';
-		usernameElement.replaceWith(newAuthorNameHTML);
-		//alert(currentAuthorNameHTML);
-		
 		// Link the author avatar with the URL scheme
-		var currentAuthorHTML = $('img.profile-picture:first').outerHTML();
-		var newAuthorHTML = '<a href=\"x-mobelux-carousel://openuser?userID=' + authorID + '\" title=\"View ' + usernameText + ' in Carousel\">' + currentAuthorHTML + '</a>';
-		$('img.profile-picture:first').replaceWith(newAuthorHTML);
+		var currentUserNameImageHTML = $('.user-avatar:first').outerHTML();
+		var newUserNameImageHTML = '<a href=\"x-mobelux-carousel://openuser?userID=' + authorID + '\" title=\"View in Carousel\">' + currentUserNameImageHTML + '</a>';
+		$('.user-avatar').replaceWith(newUserNameImageHTML);
    }
 }
 
